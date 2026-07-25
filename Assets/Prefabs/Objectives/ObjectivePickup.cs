8bd5395bc92c289a95fd400f87606426802d2a38
Ryan Lee <icopygames1@gmail.com>
2026-07-25 13:49:43 -0700

    Added sounds

--- Assets/Prefabs/Objectives/ObjectivePickup.cs
+++ Assets/Prefabs/Objectives/ObjectivePickup.cs
@@ -4,33 +4,33 @@ using UnityEngine;
 
 public class ObjectivePickup : MonoBehaviour
 {
-
     public ExitController door;
+
+    [Header("Pickup sound")]
+    public AudioClip pickupClip;
+    [Range(0f, 1f)] public float pickupVolume = 0.8f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
-        //Debug.Log(collision.tag);
-        if(collision.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
-
             door.collectObj();
 
-            //Debug.Log("collect");
+            // Play at the pickup's position so it survives the object being disabled.
+            if (pickupClip)
+                AudioSource.PlayClipAtPoint(pickupClip, transform.position, pickupVolume);
 
             gameObject.SetActive(false);
         }
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         door = Object.FindFirstObjectByType<ExitController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
